5241e41a795dbb55d174eeed11cafb36da164ceb
BeeMoe5 <48176165+BeeMoe5@users.noreply.github.com>
2022-06-12 19:30:10 -0400

    added UserConnection model

--- src/models.py
+++ src/models.py
@@ -1,10 +1,22 @@
 from pydantic import BaseModel
+from starlette.websockets import WebSocket
 
 
 class User(BaseModel):
     name: str
     current_world: str
     id: int
+
+
+class UserConnection:
+    def __init__(self, user: User, ws: WebSocket):
+        self.user = user
+        self.ws = ws
+
+    async def send_event(
+        self, event: "Event"
+    ):  # the type of event is Event from events.py
+        await self.ws.send_json(event)
 
 
 class Message(BaseModel):
