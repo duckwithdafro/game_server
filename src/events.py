b2b3e9dc3cbc0ef3b0cf8b1cc37aeb4657c81853
BeeMoe5 <48176165+BeeMoe5@users.noreply.github.com>
2022-06-12 19:29:04 -0400

    added payload models and event models
    for better readability, scalability, docs, and validation

--- src/events.py
+++ src/events.py
@@ -2,6 +2,8 @@ from enum import Enum
 
 from pydantic import BaseModel
 
+from models import Message, User
+
 
 class EventType(Enum):
     USER_JOIN = "user_join"
@@ -9,7 +11,38 @@ class EventType(Enum):
     MESSAGE = "message"
 
 
+class PayloadBase(BaseModel):
+    pass
+
+
+class UserJoinPayload(PayloadBase):
+    user: User
+
+
+class UserLeavePayload(PayloadBase):
+    user: User
+
+
+class MessagePayload(PayloadBase):
+    message: Message
+
+
 class Event(BaseModel):
     type: EventType
     # payload can contain different data depending on the event type
-    payload: dict
+    payload: PayloadBase
+
+
+class UserJoinEvent(Event):
+    type = EventType.USER_JOIN
+    payload: UserJoinPayload
+
+
+class UserLeaveEvent(Event):
+    type = EventType.USER_LEAVE
+    payload: UserLeavePayload
+
+
+class MessageEvent(Event):
+    type = EventType.MESSAGE
+    payload: MessagePayload
